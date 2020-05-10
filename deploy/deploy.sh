dc8d766b71261a66b78b470478a0987f92b242db
dqn <suyarizzz@gmail.com>
2020-05-11 04:04:28 +0900

    fix: deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,5 +5,6 @@
 eval "$(cat ./deploy/.env <(echo) <(declare -x))"
 
 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w"
+ssh -t ${USER_NAME}@${HOST} -p ${PORT} "sudo systemctl stop example.service"
 scp -P ${PORT} example ${USER_NAME}@${HOST}:/home/${USER_NAME}/
 ssh -t ${USER_NAME}@${HOST} -p ${PORT} "sudo systemctl restart example.service"
